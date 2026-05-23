d61b2301e9cd7b8b8de8ddd1d18968a197c7d6f1
amandakikuta <amanda.kikuta@ufms.br>
2026-05-23 17:42:03 -0300

    Funcionalidades do Telegram adicionadas

--- src/main.py
+++ src/main.py
@@ -10,293 +10,76 @@
 # Em outras palavras:
 # quando quisermos iniciar o JARVIS Acadêmico, vamos rodar este
 # arquivo.
-#
-# Por enquanto, este arquivo vai criar um chat simples no terminal.
-# O usuário digita uma mensagem, o sistema envia essa mensagem
-# para a LLM e depois mostra a resposta na tela.
 # ============================================================
-#
-#
-# ------------------------------------------------------------
-# Importamos a função chamar_llm.
-#
-# Essa função envia uma mensagem diretamente para o Gemma e recebe
-# uma resposta textual.
-#
-# Ela será usada quando a mensagem do usuário NÃO precisar chamar
-# nenhuma ferramenta.
-#
-# Exemplo:
-#
-# Usuário:
-# "Explique rapidamente o que é uma LLM."
-#
-# Nesse caso, o sistema pode responder diretamente com a LLM, sem
-# consultar agenda, tarefas ou materiais.
-# ------------------------------------------------------------
-from src.llm_client import chamar_llm
 
 
 # ------------------------------------------------------------
-# Importamos a função recuperar_decisao_ferramenta.
-#
-# Essa função pede ao Gemma para decidir se a mensagem do usuário
-# precisa chamar alguma ferramenta.
-#
-# Ela devolve um dicionário Python no formato:
-#
-# {
-#     "usar_ferramenta": True,
-#     "nome_ferramenta": "consultar_tarefas",
-#     "entrada": {}
-# }
-#
-# Essa decisão será usada no fluxo principal do chat.
+# Importamos o objeto Application e a função gerenciar_comunicação
+# do arquivo telegram.py.
 # ------------------------------------------------------------
-from src.llm_client import recuperar_decisao_ferramenta
+from src.telegram import application, gerenciar_comunicacao
 
 
 # ------------------------------------------------------------
-# Importamos a função carregar_descricoes_ferramentas.
+# Importamos as classe MessageHandler e filters do submódulo
+# telegram.ext.
 #
-# Essa função retorna a lista de ferramentas disponíveis no sistema,
-# com nome, descrição e parâmetros.
+# A classe MessageHandler é utilizada para lidar com mensagens
+# que não são comandos (ex: texto comum enviado pelo usuário).
+# Ela permite definir qual função será executada quando uma
+# mensagem for recebida.
 #
-# O Gemma precisa receber essas descrições para saber quais
-# ferramentas ele pode escolher.
+# O módulo filters fornece filtros prontos que permitem selecionar
+# quais tipos de mensagens o handler deve processar, como por
+# exemplo apenas texto, apenas comandos, imagens, etc.
 # ------------------------------------------------------------
-from src.tools import carregar_descricoes_ferramentas
-
-
-# ------------------------------------------------------------
-# Importamos a função executar_ferramenta.
-#
-# Essa função recebe:
-#
-# - nome da ferramenta;
-# - entrada da ferramenta.
-#
-# Depois ela:
-#
-# - recupera a função Python correta;
-# - executa essa função;
-# - registra o log;
-# - devolve a saída.
-# ------------------------------------------------------------
-from src.tools import executar_ferramenta
-
+from telegram.ext import MessageHandler, filters
 #endregion
 
 #region função main
 # ------------------------------------------------------------
 # Função: main
 # ------------------------------------------------------------
-# Esta função representa o fluxo principal do programa.
-#
-# Ela será responsável por:
-#
-# - iniciar o chat;
-# - receber mensagens do usuário;
-# - enviar essas mensagens para a LLM;
-# - imprimir a resposta da LLM;
-# - encerrar o programa quando o usuário digitar "sair".
+# 
 # ------------------------------------------------------------
 def main():
-    # --------------------------------------------------------
-    # Aqui mostramos uma mensagem inicial no terminal.
+    # ------------------------------------------------------------
+    # Aqui configuramos para que quando o usuário enviar uma mensagem
+    # de texto que não seja um comando, execute a função 
+    # gerenciar_comunicacao.
     #
-    # Isso serve para avisar ao usuário que o sistema começou
-    # corretamente.
+    # application.add_handler() é responsável por adicionar uma
+    # regra de comportamento (add_handler registra um handler no bot).
     #
-    # O comando print(...) mostra texto na tela.
-    # --------------------------------------------------------
-    print("JARVIS Acadêmico iniciado.")
-    print("Digite 'sair' para encerrar o programa.")
-
-    # --------------------------------------------------------
-    # Agora carregamos as descrições das ferramentas disponíveis.
+    # MessageHandler é a classe utilizada para tratar mensagens
+    # comuns. Ela precisa de um filtro que determinará quando
+    # a função deverá ser executada e uma função que será executada
+    # quando as condições forem satisfeitas.
     #
-    # Essas descrições serão enviadas para o Gemma sempre que ele
-    # precisar decidir se deve chamar alguma ferramenta.
+    # filter.TEXT seleciona apenas mensagens que são textos e
+    # ~filter.COMMAND seleciona tudo que não é comando ('~' indica
+    # negação).
     #
-    # Exemplos de ferramentas descritas:
-    #
-    # - consultar_tarefas;
-    # - adicionar_tarefa;
-    # - concluir_tarefa;
-    # - consultar_agenda_hoje;
-    # - consultar_agenda_amanha.
-    #
-    # Carregamos isso uma vez antes do loop porque a lista de
-    # ferramentas não muda enquanto o programa está rodando.
-    # --------------------------------------------------------
-    descricoes_ferramentas = carregar_descricoes_ferramentas()
+    # Assim, a função gerenciar_comunicacao será executada quando
+    # a condição do filtro, que nesse caso é para quando as mensagens
+    # são textos comuns E (&) não são comandos, for satisfeito.
+    # ------------------------------------------------------------
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, gerenciar_comunicacao))
 
-    # --------------------------------------------------------
-    # Aqui começa um loop infinito.
-    #
-    # Um loop é uma repetição.
-    #
-    # while True significa:
-    #
-    # "repita isso para sempre, até que alguma parte do código
-    # mande parar".
-    #
-    # Usamos isso porque um chat precisa continuar funcionando
-    # enquanto o usuário quiser conversar.
-    # --------------------------------------------------------
-    while True:
-        # ----------------------------------------------------
-        # O comando input(...) espera o usuário digitar algo
-        # no terminal.
-        #
-        # O texto "Você: " aparece só para indicar onde o usuário
-        # deve escrever a mensagem.
-        #
-        # Tudo que o usuário digitar será guardado na variável
-        # chamada "mensagem".
-        # ----------------------------------------------------
-        mensagem = input("\nVocê: ")
+    # ------------------------------------------------------------
+    # Imprimimos uma mensagem para avisar que o bot está pronto
+    # para comunicação.
+    # ------------------------------------------------------------
+    print("Bot integrado e rodando...")
 
-        # ----------------------------------------------------
-        # Aqui verificamos se o usuário quer sair do programa.
-        #
-        # mensagem.lower() transforma o texto em letras minúsculas.
-        #
-        # Isso permite que o programa entenda:
-        #
-        # sair
-        # SAIR
-        # Sair
-        # sAiR
-        #
-        # como a mesma coisa.
-        # ----------------------------------------------------
-        if mensagem.lower() == "sair":
-            # ------------------------------------------------
-            # Se o usuário digitou "sair", mostramos uma mensagem
-            # de encerramento.
-            # ------------------------------------------------
-            print("JARVIS: Até mais!")
-
-            # ------------------------------------------------
-            # O break serve para interromper o loop.
-            #
-            # Como o loop estava repetindo para sempre, o break
-            # é o comando que manda parar.
-            # ------------------------------------------------
-            break
-
-        # ----------------------------------------------------
-        # Agora vamos pedir para a LLM decidir se esta mensagem
-        # precisa chamar alguma ferramenta.
-        #
-        # Exemplos:
-        #
-        # "Liste minhas tarefas."
-        #   -> deve usar a ferramenta consultar_tarefas.
-        #
-        # "O que tenho hoje?"
-        #   -> deve usar a ferramenta consultar_agenda_hoje.
-        #
-        # "Explique rapidamente o que é uma LLM."
-        #   -> talvez não precise de ferramenta.
-        #
-        # A função recuperar_decisao_ferramenta(...) devolve um
-        # dicionário Python com a decisão da LLM.
-        #
-        # Exemplo:
-        #
-        # {
-        #     "usar_ferramenta": True,
-        #     "nome_ferramenta": "consultar_tarefas",
-        #     "entrada": {}
-        # }
-        # ----------------------------------------------------
-        decisao = recuperar_decisao_ferramenta(
-            mensagem_usuario=mensagem,
-            descricoes_ferramentas=descricoes_ferramentas
-        )
-
-        # ----------------------------------------------------
-        # Agora verificamos se a LLM decidiu usar uma ferramenta.
-        #
-        # decisao.get("usar_ferramenta", False)
-        #
-        # significa:
-        #
-        # pegue o valor da chave "usar_ferramenta".
-        #
-        # Se essa chave não existir por algum motivo, use False
-        # como valor padrão.
-        # ----------------------------------------------------
-        if decisao.get("usar_ferramenta", False):
-            # ------------------------------------------------
-            # Se chegamos aqui, significa que a LLM decidiu que
-            # alguma ferramenta deve ser chamada.
-            #
-            # Agora recuperamos o nome da ferramenta escolhida.
-            #
-            # Exemplo:
-            #
-            # "consultar_tarefas"
-            # ------------------------------------------------
-            nome_ferramenta = decisao.get("nome_ferramenta")
-
-            # ------------------------------------------------
-            # Agora recuperamos a entrada que será enviada para a
-            # ferramenta.
-            #
-            # Exemplo para consultar_tarefas:
-            #
-            # {}
-            #
-            # Exemplo para adicionar_tarefa:
-            #
-            # {
-            #     "titulo": "Estudar RAG",
-            #     "descricao": ""
-            # }
-            # ------------------------------------------------
-            entrada = decisao.get("entrada", {})
-
-            # ------------------------------------------------
-            # Agora executamos a ferramenta escolhida.
-            #
-            # A função executar_ferramenta(...) faz várias coisas:
-            #
-            # 1. encontra a função Python correta;
-            # 2. executa essa função com a entrada recebida;
-            # 3. registra a chamada no log;
-            # 4. devolve a saída da ferramenta.
-            # ------------------------------------------------
-            resposta = executar_ferramenta(
-                nome_ferramenta=nome_ferramenta,
-                entrada=entrada
-            )
-
-            # ------------------------------------------------
-            # Mostramos ao usuário a resposta devolvida pela
-            # ferramenta.
-            # ------------------------------------------------
-            print(f"\nJARVIS: {resposta}")
-
-        else:
-            # ------------------------------------------------
-            # Se chegamos aqui, significa que a LLM decidiu que
-            # nenhuma ferramenta é necessária.
-            #
-            # Então usamos o comportamento antigo:
-            # mandamos a mensagem diretamente para o Gemma gerar
-            # uma resposta normal.
-            # ------------------------------------------------
-            resposta = chamar_llm(mensagem)
-
-            # ------------------------------------------------
-            # Mostramos a resposta normal da LLM no terminal.
-            # ------------------------------------------------
-            print(f"\nJARVIS: {resposta}")
-
+    # ------------------------------------------------------------
+    # Aqui inicializamos o bot e o event loop, isto é, fazemos com
+    # que o bot fique perguntando repetidamente para o Telegram se
+    # há uma mensagem nova. Assim, quando uma mensagem é recebida,
+    # cria-se um Update e o envia para o handler correto.
+    # ------------------------------------------------------------
+    application.run_polling()
+            
 #endregion
 
 #region execução do programa
